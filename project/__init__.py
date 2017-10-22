ccb24453a60df83d201c5da6735cac7387cfe29e
joshuaai <joshuaiheji@gmaill.com>
2017-10-22 22:50:39 +0000

    Setting up Docker image for Flask app

--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,15 @@
 """Initialize Flask and configuration"""
-
+import os
 from flask import Flask, jsonify
 
 # instantiate the app
 APP = Flask(__name__)
 
 # set the config
-APP.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+APP.config.from_object(app_settings)
+
+# print(APP.config)
 
 @APP.route('/ping', methods=['GET'])
 def ping_pong():
